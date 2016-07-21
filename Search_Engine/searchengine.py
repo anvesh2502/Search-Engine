6be8d3336527b4207d102bc851a4df67ccd8cc18
anvesh2502 <anveshrocks@yahoo.co.in>
2016-07-21 16:05:26 -0500

    Added the crawler function

--- Search_Engine/searchengine.py
+++ Search_Engine/searchengine.py
@@ -1,4 +1,9 @@
+import urllib2
+from BeautifulSoup import *
+from urlparse import urljoin
 
+# Words which are ignored by the crawler
+ignorewords=set(['the','of','to','and','a','in','is','it'])
 
 # Adding a crawler class
 class crawler :
@@ -42,10 +47,38 @@ class crawler :
     # as we go
 
     def crawl(self,pages,depth=2) :
-        pass
+        for i in range(depth) :
+            newpages=set()
+            for page in pages :
+                try :
+                    c=urllib2.urlopen(page)
+                except :
+                    print 'Could not open %s'%page
+                    continue
+                soup=BeautifulSoup(c.read())
+                self.addtoindex(page,soup)
+
+
+                links=soup('a')
+                for link in links :
+                    if ('href' in dict(link.attrs)) :
+                        url=urljoin(page,link['href'])
+                        if url.find("'")!=-1 : continue
+                        url=url.split('#')[0]
+                        if url[0:4]=='http' and not self.isindexed(url) :
+                            newpages.add(url)
+                        linkText=self.gettextonly(link)
+                        self.addlinkref(page,url,linkText)
+                self.dbcommit()
+            pages=newpages
 
     # Create the database tables
     def createindextables(self) :
         pass
 
-        
+
+
+
+pagelist=['http://www.forbes.com/']
+crawler=crawler('')
+crawler.crawl(pagelist)
